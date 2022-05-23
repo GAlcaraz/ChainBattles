5290acf81e3810918a95ec1d712db2422205f7ab
GAlcaraz <g.alcaraz@outlook.com>
2022-05-23 00:00:32 -0300

    Update hardhat.config to common js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,16 @@
-import "dotenv";
-import "@nomiclabs/hardhat-waffle";
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-export const solidity = "0.8.10";
-export const networks = {
-  mumbai: {
-    url: process.env.TESTNET_RPC,
-    accounts: [process.env.PRIVATE_KEY],
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
-};
-export const etherscan = {
-  apiKey: process.env.POLYGONSCAN_API_KEY,
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  },
 };
